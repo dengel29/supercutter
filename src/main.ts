e8176db9ecd591c6f394c7e12fc0418e7ab09f83
Dan Engel <dengel29@gmail.com>
2020-12-23 16:35:38 +0800

    successfully downloads and cuts based on keyword. keyword is hardcoded, and youtube video is found in parameters. need to hashout user flow for finding video -> seeing subtitles -> generating video

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { getSubtitles } from 'youtube-captions-scraper';
+import fs, { rename } from 'fs';
 import path from 'path';
 import express from 'express';
 import nunjucks from 'nunjucks';
 import axios from 'axios';
 import he from 'he';
 import striptags from 'striptags'
+import ytdl from 'ytdl-core';
+import ffmpeg from 'fluent-ffmpeg';
 
 
+const download = true;
 const app = express()
 const port = 3000;
 nunjucks.configure(path.join(__dirname, 'views'), {
@@ -15,15 +18,15 @@ nunjucks.configure(path.join(__dirname, 'views'), {
   watch: true
 });
 
-app.get('/v/:videoId', (req, res) => {
-  console.log(req.params)
-  getSubtitles({
-    videoID: req.params.videoId, // youtube video id
-    lang: 'en' // default: `en`
-  }).then((captions: JSON) => {
-    res.send(captions)
-  });
-})
+// app.get('/v/:videoId', (req, res) => {
+//   console.log(req.params)
+//   getSubtitles({
+//     videoID: req.params.videoId, // youtube video id
+//     lang: 'en' // default: `en`
+//   }).then((captions: JSON) => {
+//     res.send(captions)
+//   });
+// })
 
 app.get('/p/:videoId/:lang', async (req, res) => {
 
@@ -34,20 +37,21 @@ const { data } = await axios.get(
   `https://youtube.com/get_video_info?video_id=${vs.videoId}`
 );
 
-const decodedData: string = decodeURIComponent(data);
-
+  const decodedData: string = decodeURIComponent(data);
   // * ensure we have access to captions data
   if (!decodedData.includes('captionTracks'))
     throw new Error(`Could not find captions for video: ${vs.videoId}`);
 
   const regex = /({"captionTracks":.*isTranslatable":(true|false)}])/;
-  const [match] = regex.exec(decodedData);
+  const [match]: RegExpExecArray = regex.exec(decodedData);
   const titleRegex = /(?="title":{"simpleText":).*(?=},"description")/;
-  const videoTitle = titleRegex.exec(decodedData)[0].replace(`"title":{"simpleText":`, '').replace(/\+/g, ' ')
+  const videoTitle = titleRegex.exec(decodedData)[0].replace(`"title":{"simpleText":`, '').replace(/\+/g, ' ').replace(/"/g, '')
   const thumbsRegex = /(?={"thumbnails":).*(?=,"averageRating)/;
-  const thumbsMatch: string = thumbsRegex.exec(decodedData)[0]
-  const thumbsObject: {thumbnails: [{url:string, width: number, height: number}]} = JSON.parse(thumbsMatch)
-  console.log(thumbsObject.thumbnails)
+  const [thumbsMatch]: RegExpExecArray = thumbsRegex.exec(decodedData)
+
+  type ThumbsObject = {thumbnails: [{url:string, width: number, height: number}]}
+  const thumbsObj: ThumbsObject = JSON.parse(thumbsMatch)
+  const thumbnails = thumbsObj.thumbnails
   // console.log(thumbsMatch.thumbnails)
   // const [titleMatch] = titleRegex.exec(decodedData);
   const { captionTracks } = JSON.parse(`${match}}`);
@@ -65,14 +69,18 @@ const decodedData: string = decodeURIComponent(data);
 ]
 */
   const vidData = captionTracks.find( ({ vssId })  => vssId === `.${vs.lang}` || vssId === `a.${vs.lang}` || vssId && vssId.match(`.${vs.lang}`))
-
+  type CCBlock = {
+    start: number,
+    dur: number,
+    text: string,
+  }
   // * ensure we have found the correct vidData lang
   if (!vidData || (vidData && !vidData.baseUrl))
     throw new Error(`Could not find ${vs.lang} captions for ${vs.videoId}`);
 
 
     const { data: transcript } = await axios.get(vidData.baseUrl);
-    const lines = transcript
+    let lines: Array<CCBlock> = transcript
     .replace('<?xml version="1.0" encoding="utf-8" ?><transcript>', '')
     .replace('</transcript>', '')
     .split('</text>')
@@ -99,19 +107,86 @@ const decodedData: string = decodeURIComponent(data);
         text,
       };
     });
+    lines = lines.filter(line => line.text.includes('bounce'))
+    let videoCutInstructions = "select='"
+    let audioCutInstructions = "aselect='"
+    for (const line of lines) {
+      //select='between(t,4,6.5)+between(t,17,26)+between(t,74,91)
+      const timesString = `between(t,${line.start},${Number(line.start)+Number(line.dur)})+`
+      videoCutInstructions = videoCutInstructions.concat(timesString)
+      audioCutInstructions = audioCutInstructions.concat(timesString)
+
+    }
+    videoCutInstructions = videoCutInstructions.slice(0, -1).concat('\', setpts=N/FRAME_RATE/TB');
+    audioCutInstructions = audioCutInstructions.slice(0, -1).concat('\', asetpts=\'N/SR/TB\'');
+    console.log(videoCutInstructions)
+    console.log(audioCutInstructions)
     const pageData = {
       title: 'Subs for your video',
       videoTitle: videoTitle,
       subtitleLines: lines,
-      // https://www.youtube.com/watch?v=QPjhAZd1RLI&t=12304s
+      thumbs: thumbnails,
       videoId: vs.videoId,
       videoUrl: `https://www.youtube.com/watch?v=${vs.videoId}`
 
     }
+    
+    if (download) {
+
+      await downloadFile(`https://www.youtube.com/watch?v=${vs.videoId}`)
+      const tempPath = './temp/temp.mp4'
+      const permPath = `./temp/${videoTitle}.mp4`
+      rename(tempPath, permPath, async () => {
+        console.log('renamed the shit, no going to cut the video')
+        await cutVideo(permPath, videoCutInstructions, audioCutInstructions)
+      });
+    }
+    
     res.render('index.html', pageData)
     return lines;
   })
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+  async function downloadFile(downloadURL: string) {
+    return new Promise( (resolve, reject) => {
+      const stream = fs.createWriteStream('./temp/temp.mp4')
+      ytdl(downloadURL).pipe(stream)
+      stream.on('finish', resolve);
+      stream.on('error', reject);
+    })
+  }
+
+  /* 
+  translate this to the fluent-ffmpeg 
+  ffmpeg -i video \
+      -vf "select='between(t,4,6.5)+between(t,17,26)+between(t,74,91)',
+          setpts=N/FRAME_RATE/TB" \
+      -af "aselect='between(t,4,6.5)+between(t,17,26)+between(t,74,91)',
+          asetpts=N/SR/TB" out.mp4
+  source: https://stackoverflow.com/questions/50594412/cut-multiple-parts-of-a-video-with-ffmpeg
+  */
+ 
+  async function cutVideo(videoPath: string, videoCutInstructions: string, audioCutInstructions: string) {
+    console.log("Inside cutvideo function", videoPath)
+    // working harcoded version, for reference
+    // ffmpeg(videoPath)
+    //   .outputOptions(
+    //     "-vf", "select='between(t,4,6.5)+between(t,17,26)+between(t,74,91)', setpts=N/FRAME_RATE/TB", 
+    //     "-af", "aselect='between(t,4,6.5)+between(t,17,26)+between(t,74,91)', asetpts='N/SR/TB'"
+    //   )
+    //   .on('end', () => console.log("we've done it"))
+    //   .on('error', (err) => console.error(err))
+    //   .save('./cuts/trimmed.mp4')
+    
+    ffmpeg(videoPath)
+      .outputOptions(
+        "-vf", `${videoCutInstructions}`, 
+        "-af", `${audioCutInstructions}`
+      )
+      .on('end', () => console.log("we've done it"))
+      .on('error', (err) => console.error(err))
+      .save('./cuts/trimmed.mp4')
+  }
 
 // }
 
