d6dfe5690d5d28664948ade3ee22176c13b50856
Dan Engel <dengel29@gmail.com>
2020-12-28 12:32:44 +0800

    gets captions  and video data, filters subtitles, ready for download function

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import striptags from 'striptags'
 import ytdl from 'ytdl-core';
 import ffmpeg from 'fluent-ffmpeg';
 
-const download = false;
+// const download = false;
 
 const app = express()
 const port = 3000;
@@ -41,9 +41,9 @@ type CCBlock = {
 //     res.send(captions)
 //   });
 // })
-type VideoRequestString = {videoId: string, lang: string}
+type VideoRequestString = {videoID: string, lang: string}
 
-async function getVideoInfo(url:string): Promise<string> {
+async function getDecodedVideoInfo(url:string): Promise<string> {
   const { data } = await axios.get(
   `https://youtube.com/get_video_info?video_id=${url}`
 );
@@ -60,35 +60,36 @@ app.post('/search-video/:videoURLOrID', async (req,res) => {
   const userInput: string = req.params.videoURLOrID;
   try {
     const videoID: string = ytdl.getVideoID(userInput)
-    console.log(userInput)
-    console.log(videoID)
-    res.send(JSON.stringify({found: 'true'}))
+    const videoData = await getVideoData(videoID)
+    res.send(JSON.stringify({videoData: videoData}))
   } catch(err) {
-    if (err == `No video id found: ${userInput}`)
-    res.send(JSON.stringify({message: "Please enter a youtube video ID or a full YouTube URL"}))
+    if (err.message == `No video id found: ${userInput}`)
+    res.send(JSON.stringify({errorMessage: "Please enter a youtube video ID or a full YouTube URL"}))
   }
 })
 
 app.get('/p/:videoId/:lang', async (req, res) => {
 
 
-// given the video URL or ID, get video info thumbnails, title, subtitles
-  const vs: VideoRequestString = {videoId: req.params.videoId, lang: req.params.lang}
-  const decodedData = await getVideoInfo(vs.videoId)
+// given the video URL or ID, get video info thumbnails, title, captions
+  const vs: VideoRequestString = {videoID: req.params.videoID, lang: req.params.lang}
+  const decodedData = await getDecodedVideoInfo(vs.videoID)
   // ensure the decoded data has the captionTracks info
   if (!decodedData.includes('captionTracks'))
-    throw new Error(`Could not find captions for video: ${vs.videoId}`);
-  /* captionTracks looks like this
-  [
-    {
-      baseUrl: 'https://www.youtube.com/api/timedtext?v=QPjhAZd1RLI&asr_langs=de,en,es,fr,it,ja,ko,nl,pt,ru&caps=asr&exp=xftt&xorp=true&xoaf=5&hl=zh-TW&ip=0.0.0.0&ipbits=0&expire=1608306851&sparams=ip,ipbits,expire,v,asr_langs,caps,exp,xorp,xoaf&signature=C6D2F64D9325491770F7F63CBC02E4587DC243A3.EB016D9708A28DDECF08A80ECE640018816F0F4C&key=yt8&kind=asr&lang=en',
-      name: { simpleText: '英文+(自動產生)' },
-      vssId: 'a.en',
-      languageCode: 'en',
-      kind: 'asr',
-      isTranslatable: true
-    }
-  ]
+    throw new Error(`Could not find captions for video: ${vs.videoID}`);
+
+  /* captionTracks look like this: 
+    captionTracks looks like this
+    [
+      {
+        baseUrl: 'https://www.youtube.com/api/timedtext?v=QPjhAZd1RLI&asr_langs=de,en,es,fr,it,ja,ko,nl,pt,ru&caps=asr&exp=xftt&xorp=true&xoaf=5&hl=zh-TW&ip=0.0.0.0&ipbits=0&expire=1608306851&sparams=ip,ipbits,expire,v,asr_langs,caps,exp,xorp,xoaf&signature=C6D2F64D9325491770F7F63CBC02E4587DC243A3.EB016D9708A28DDECF08A80ECE640018816F0F4C&key=yt8&kind=asr&lang=en',
+        name: { simpleText: '英文+(自動產生)' },
+        vssId: 'a.en',
+        languageCode: 'en',
+        kind: 'asr',
+        isTranslatable: true
+      }
+    ]
   */
   
   // get caption tracks or throw an error
@@ -98,7 +99,86 @@ app.get('/p/:videoId/:lang', async (req, res) => {
   const vidData = captionTracks.find( ({ vssId })  => vssId === `.${vs.lang}` || vssId === `a.${vs.lang}` || vssId && vssId.match(`.${vs.lang}`))
   // * ensure we have found the correct vidData lang
   if (!vidData || (vidData && !vidData.baseUrl))
-    throw new Error(`Could not find ${vs.lang} captions for ${vs.videoId}`);
+    throw new Error(`Could not find ${vs.lang} captions for ${vs.videoID}`);
+
+
+  // get other video data after we know we can get captions
+  //// title
+  const titleRegex = /(?="title":{"simpleText":).*(?=},"description")/;
+  const videoTitle = titleRegex.exec(decodedData)[0].replace(`"title":{"simpleText":`, '').replace(/\+/g, ' ').replace(/"/g, '')
+  
+  //// thumbnails
+  const thumbsRegex = /(?={"thumbnails":).*(?=,"averageRating)/;
+  const [thumbsMatch]: RegExpExecArray = thumbsRegex.exec(decodedData)
+  type ThumbsObject = {thumbnails: [{url:string, width: number, height: number}]}
+  const thumbsObj: ThumbsObject = JSON.parse(thumbsMatch)
+  const thumbnails = thumbsObj.thumbnails
+  // console.log(thumbsMatch.thumbnails)
+  // const [titleMatch] = titleRegex.exec(decodedData);
+
+  // convert XML transcript into an array of CCBlocks
+  const lines: Array<CCBlock> = await getTranscript(vidData.baseUrl)
+  
+  // create video and audio cut instructions for ffmpeg based on search word
+  
+  const download = false;
+  if (download) {
+    const keyword = 'greatest'
+    const {videoCutInstructions, audioCutInstructions} = createFFMPEGInstructions(lines, keyword)
+    await downloadFile(`https://www.youtube.com/watch?v=${vs.videoID}`)
+    const tempPath = './temp/temp.mp4'
+    const permPath = `./temp/${videoTitle}.mp4`
+    rename(tempPath, permPath, async () => {
+      console.log('renamed the shit, start cutting the video')
+      await cutVideo(permPath, videoCutInstructions, audioCutInstructions)
+    });
+  }
+  
+  const pageData = {
+    title: 'Subs for your video',
+    videoTitle: videoTitle,
+    captions: lines,
+    thumbs: thumbnails,
+    videoId: vs.videoID,
+    videoUrl: `https://www.youtube.com/watch?v=${vs.videoID}`
+
+  }
+    
+    
+    res.render('index.njk', pageData)
+    return lines;
+  })
+
+async function getVideoData(videoID: string, lang = 'en') {
+  const download = false
+  const vs: VideoRequestString = {videoID: videoID, lang: lang}
+  const decodedData = await getDecodedVideoInfo(vs.videoID)
+  // ensure the decoded data has the captionTracks info
+  if (!decodedData.includes('captionTracks'))
+    throw new Error(`Could not find captions for video: ${vs.videoID}`);
+
+  /* captionTracks look like this: 
+    captionTracks looks like this
+    [
+      {
+        baseUrl: 'https://www.youtube.com/api/timedtext?v=QPjhAZd1RLI&asr_langs=de,en,es,fr,it,ja,ko,nl,pt,ru&caps=asr&exp=xftt&xorp=true&xoaf=5&hl=zh-TW&ip=0.0.0.0&ipbits=0&expire=1608306851&sparams=ip,ipbits,expire,v,asr_langs,caps,exp,xorp,xoaf&signature=C6D2F64D9325491770F7F63CBC02E4587DC243A3.EB016D9708A28DDECF08A80ECE640018816F0F4C&key=yt8&kind=asr&lang=en',
+        name: { simpleText: '英文+(自動產生)' },
+        vssId: 'a.en',
+        languageCode: 'en',
+        kind: 'asr',
+        isTranslatable: true
+      }
+    ]
+  */
+  
+  // get caption tracks or throw an error
+  const regex = /({"captionTracks":.*isTranslatable":(true|false)}])/;
+  const [match]: RegExpExecArray = regex.exec(decodedData);
+  const { captionTracks } = JSON.parse(`${match}}`);
+  const vidData = captionTracks.find( ({ vssId })  => vssId === `.${vs.lang}` || vssId === `a.${vs.lang}` || vssId && vssId.match(`.${vs.lang}`))
+  // * ensure we have found the correct vidData lang
+  if (!vidData || (vidData && !vidData.baseUrl))
+    throw new Error(`Could not find ${vs.lang} captions for ${vs.videoID}`);
 
 
   // get other video data after we know we can get captions
@@ -124,7 +204,7 @@ app.get('/p/:videoId/:lang', async (req, res) => {
   if (download) {
     const keyword = 'greatest'
     const {videoCutInstructions, audioCutInstructions} = createFFMPEGInstructions(lines, keyword)
-    await downloadFile(`https://www.youtube.com/watch?v=${vs.videoId}`)
+    await downloadFile(`https://www.youtube.com/watch?v=${vs.videoID}`)
     const tempPath = './temp/temp.mp4'
     const permPath = `./temp/${videoTitle}.mp4`
     rename(tempPath, permPath, async () => {
@@ -133,20 +213,17 @@ app.get('/p/:videoId/:lang', async (req, res) => {
     });
   }
   
-  const pageData = {
+  return {
     title: 'Subs for your video',
     videoTitle: videoTitle,
-    subtitleLines: lines,
+    captions: lines,
     thumbs: thumbnails,
-    videoId: vs.videoId,
-    videoUrl: `https://www.youtube.com/watch?v=${vs.videoId}`
+    videoID: vs.videoID,
+    videoURL: `https://www.youtube.com/watch?v=${vs.videoID}`
 
   }
-    
-    
-    res.render('index.njk', pageData)
-    return lines;
-  })
+}
+
 
   async function getTranscript(videoDataURL: string): Promise<Array<CCBlock>> {
     const { data: transcript } = await axios.get(videoDataURL);
@@ -217,15 +294,17 @@ app.get('/p/:videoId/:lang', async (req, res) => {
   */
   async function cutVideo(videoPath: string, videoCutInstructions: string, audioCutInstructions: string) {
     console.log("Inside cutvideo function", videoPath)
-    // working harcoded version, for reference
-    // ffmpeg(videoPath)
-    //   .outputOptions(
-    //     "-vf", "select='between(t,4,6.5)+between(t,17,26)+between(t,74,91)', setpts=N/FRAME_RATE/TB", 
-    //     "-af", "aselect='between(t,4,6.5)+between(t,17,26)+between(t,74,91)', asetpts='N/SR/TB'"
-    //   )
-    //   .on('end', () => console.log("we've done it"))
-    //   .on('error', (err) => console.error(err))
-    //   .save('./cuts/trimmed.mp4')
+    /* 
+      working harcoded version, for reference
+      ffmpeg(videoPath)
+        .outputOptions(
+          "-vf", "select='between(t,4,6.5)+between(t,17,26)+between(t,74,91)', setpts=N/FRAME_RATE/TB", 
+          "-af", "aselect='between(t,4,6.5)+between(t,17,26)+between(t,74,91)', asetpts='N/SR/TB'"
+        )
+        .on('end', () => console.log("we've done it"))
+        .on('error', (err) => console.error(err))
+        .save('./cuts/trimmed.mp4')
+    */
     
     ffmpeg(videoPath)
       .outputOptions(
